1de09915b8bf31f47480be2c194f1ad481d5300b
deepanshu <deepanshu2307696@st.jmi.ac.in>
2024-05-26 17:15:33 +0530

    Added ESLint Config

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -7,11 +7,11 @@ const Person = require('./models/person') // import our model and store it in Pe
 // morgon is a request logger and it has many configurations, we're using tiny
 // we can also create new tokens that we can log during our request logging in the console
 // here we are logging request body in the console, just for checking purpose, altho it is v uncommon and might as well be considered a privacy nightmare
-morgan.token('body', (req,res) => {
-    return JSON.stringify(req.body) // need to return the token as string otherwise it'll pring [Object object]
+morgan.token('body', (req) => {
+  return JSON.stringify(req.body) // need to return the token as string otherwise it'll pring [Object object]
 })
 
-const now = new Date() // used to print in info 
+const now = new Date() // used to print in info
 
 app.use(express.json()) // use json parser to send requests
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body')) // create the format of the resource logging to use
@@ -19,88 +19,85 @@ app.use(express.static('dist')) // used to serve dist folder as the starting poi
 
 // this one is a middleware that runs when api request is made on a address that has nothing
 const unknownEndpoint = (req, res) => {
-    res.status(404).send({ error: 'unknown endpoint' })
+  res.status(404).send({ error: 'unknown endpoint' })
 }
-
-// persons object, make sure to use let instead of const otherwise you won't be able to make delete or post requets
-let persons = []
 
 // get all persons
 app.get('/api/persons', (req, res, next) => {
-    Person.find({})
-        .then(persons => {
-            res.json(persons)
-        })
-        .catch(error => next(error))
+  Person.find({})
+    .then(persons => {
+      res.json(persons)
+    })
+    .catch(error => next(error))
 })
 
 // get info of the phone book
 app.get('/info', (req, res, next) => {
-    Person.find({})
-        .then(persons => {
-            res.send(`Phonebook has info for ${persons.length} people <br /><br />${now}`)
-        })
-        .catch(error => next(error))
+  Person.find({})
+    .then(persons => {
+      res.send(`Phonebook has info for ${persons.length} people <br /><br />${now}`)
+    })
+    .catch(error => next(error))
 })
 
-// get single person 
+// get single person
 app.get('/api/persons/:id', (req, res, next) => {
-    Person.findById(req.params.id)
-        .then(person => {
-            if(person) {
-                res.json(person)
-            } else {
-                res.status(404).end()
-            }
-        })
-        .catch(error => next(error))
+  Person.findById(req.params.id)
+    .then(person => {
+      if(person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 // delete a person
 app.delete('/api/persons/:id', (req, res, next) => {
-    Person.findByIdAndDelete(req.params.id)
-        .then(result => {
-            res.status(204).end()
-        })
-        .catch(error => next(error))
+  Person.findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 // make a post request
 app.post('/api/persons', (req, res, next) => {
-    const body = req.body
+  const body = req.body
 
-    if (!body.phone || !body.name) {
-        return res.status(400).json({
-            error: 'content warning: name or phone is missing'
-        })
-    }
-
-    const person = new Person({
-        name: body.name,
-        phone: body.phone,
+  if (!body.phone || !body.name) {
+    return res.status(400).json({
+      error: 'content warning: name or phone is missing'
     })
+  }
 
-    person.save()
-        .then(savedPerson => {
-            res.json(savedPerson)
-        })
-        .catch(error => next(error))
+  const person = new Person({
+    name: body.name,
+    phone: body.phone,
+  })
+
+  person.save()
+    .then(savedPerson => {
+      res.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
 // update person details
 app.put('/api/persons/:id', (req, res, next) => {
-    const body = req.body
+  const body = req.body
 
-    const person = {
-        name: body.name,
-        phone: body.phone
-    }
+  const person = {
+    name: body.name,
+    phone: body.phone
+  }
 
-    Person.findByIdAndUpdate(req.params.id, person, { new: true, runValidators: true, context: 'query' })
-        .then(updatedPerson => {
-            res.json(updatedPerson)
-        })
-        .catch(error => next(error))
+  Person.findByIdAndUpdate(req.params.id, person, { new: true, runValidators: true, context: 'query' })
+    .then(updatedPerson => {
+      res.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 // make sure of the order of the middleware that you use, runs from top to bottom
@@ -108,15 +105,15 @@ app.use(unknownEndpoint)
 
 // create another middleware for error handling
 const errorHandler = (error, req, res, next) => {
-    console.error(error.message)
+  console.error(error.message)
 
-    if(error.name === 'CastError') {
-        return res.status(400).send({ error: 'malformatted id' })
-    } else if(error.name === 'ValidationError') {
-        return res.status(400).json({ error: error.message })
-    }
+  if(error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if(error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  }
 
-    next(error)
+  next(error)
 }
 
 // error handler middleware always comes last
@@ -124,5 +121,5 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`)
+  console.log(`server running on port ${PORT}`)
 })
--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -8,39 +8,39 @@ console.log('connecting to',url)
 
 // try connecting to url, print appropriate message
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch(error => {
-        console.log('error connecting to MongoDB', error.message)
-    })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB', error.message)
+  })
 
 // create the person schema, the way in which the mongodb database collection will store the appropriate object in the document
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true
-    },
-    phone: {
-        type: String,
-        validate: {
-            validator: (v) => {
-                const regex = /^\d{2,3}-\d+$/;
-                return regex.test(v) && v.length >= 8;
-            },
-            message: props => `${props.value} is not a valid phone number!`
-        }
-    },
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  phone: {
+    type: String,
+    validate: {
+      validator: (v) => {
+        const regex = /^\d{2,3}-\d+$/
+        return regex.test(v) && v.length >= 8
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    }
+  },
 })
 
 // transform the schema to remove un-needed fields and convert the id into a string (which is set to an object by default in mongodb)
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 // export module using commonjs syntax
